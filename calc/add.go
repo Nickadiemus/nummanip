132e50691b653cc3f4a92f39c46bcf9106a61d2d
nickadiemus <nicholasgsladic@gmail.com>
2021-07-12 15:19:59 -0400

    feat: add 0 <= n numbers

--- calc/add.go
+++ calc/add.go
@@ -3,3 +3,11 @@ package calc
 func Add(a, b int) int {
 	return a + b
 }
+
+func AddMany(numbers ...int) int {
+	var sum int
+	for _, number := range numbers {
+		sum = +number
+	}
+	return sum
+}
--- calc/subtract.go
+++ calc/subtract.go
@@ -1,6 +1,5 @@
 package calc
 
-func Subtract(a,b int) (int) {
+func Subtract(a, b int) int {
 	return a - b
 }
-
